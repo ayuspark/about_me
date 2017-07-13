5c1689492c35246d2345969920edcf172e545d42
ayuspark <lola.luyaowang@gmail.com>
2017-07-13 16:56:04 -0700

    dried some JS code, putting question 1-5 in an multi-dimentional array

--- app.js
+++ app.js
@@ -3,74 +3,41 @@
 var guestName = prompt("Hey, what's your name? Let's play a game!");
 var correctTimes = 0;
 
-//***using ARRAY to store questions and answers:
-//var array1 to include 1-5 questions, excluding 6 and 7, bcuz it needs more code to put them in an array(?)
-//e.g. [[question1, {y: "alert msg for answer Y, n:"alert msg for answer N"}], [question2, {}], etc
-//var answer to take user input. answer needs to be clear in each loop. Msg being prompt can be accessed by hash's key-value pair.
-
 document.getElementById('guest_name').innerHTML = guestName;
 document.getElementById('h1_guest_name').innerHTML = guestName;
 console.log("User types in name: " + guestName);
 
-// question 1
-var answer1 = prompt("Can pigs look at the sky?\nY or N?").toLowerCase();
-if(answer1 === "n" || answer1 === "no") {
-  alert("Correct! Their muscle doesn't allow that. Sad face.");
-  correctTimes ++;
-} else if (answer1 === "y" || answer1 === "yes") {
-  alert("Wrong. They cannot fly either.")
-} else {
-  alert("You didn't type in Y or N.")
+var answer;
+
+// question 1-5
+var yesOrNoQuestions = function() {
+  var q1 = "Can pigs look at the sky?",
+      q2 = "Can horses sleep while standing up?",
+      q3 = "Do baby elephants suck their trunk?",
+      q4 = "Do prairie dogs kiss?",
+      q5 = "Finally, can cats taste sugar?";
+
+  var gameArray = [[q1, {"y":["Wrong. They cannot fly either.", 0], "n":["Correct! Their muscle doesn't allow that. Sad face.", 1]}],
+                  [q2, {"y":["Correct! Horses are so cool...", 1], "n":["Wrong. Can you imagine when they cuddle?", 0]}],
+                  [q3, {"y":["Correct! They are just like babies.", 1], "n":["Wrong. There haven't yet been a pacifier for elephants.", 0]}],
+                  [q4, {"y":["Correct! They speak French too.", 1], "n":["Wrong. You are just jealous.", 0]}],
+                  [q5, {"y":["Wrong. They never smile, can't you see.", 0], "n":["Correct! They are flawed, god forbid!", 1]}]];
+
+  for (var i = 0; i < 5; i++) {
+    answer = prompt(gameArray[i][0]).toLowerCase();
+    if (answer === "y" || answer === "yes") {
+      alert(gameArray[i][1]["y"][0]);
+      correctTimes += gameArray[i][1]["y"][1];
+    } else if (answer === "n" || answer === "no") {
+      alert(gameArray[i][1]["n"][0]);
+      correctTimes += gameArray[i][1]["n"][1];
+    } else {
+      alert("You didn't type in yes or no.");
+    }
+  }
 }
-console.log("user answer1 is: " + answer1);
 
-// question 2
-var answer2 = prompt("Can horses sleep while standing up?\nY or N?").toLowerCase();
-console.log("user answer2 is: " + answer2);
-if (answer2 === "y" || answer2 === "yes") {
-  alert("Correct! Horses are so cool...");
-  correctTimes ++;
-} else if (answer2 === "n" || answer2 === "no") {
-  alert("Wrong. Can you imagine when they cuddle?")
-} else {
-  alert("You didn't type in Y or N.")
-};
-
-// question 3
-var answer3 = prompt("Do baby elephants suck their trunk?\nY or N?").toLowerCase();
-console.log("user answer3 is: " + answer3);
-if (answer3 == "y" || answer3 === "yes") {
-  alert("Correct! They are just like babies.");
-  correctTimes ++;
-} else if (answer3 == "n" || answer3 === "no") {
-  alert("Wrong. There haven't yet been a pacifier for elephants.")
-} else {
-  alert("You didn't type in Y or N.")
-};
-
-// question 4
-var answer4 = prompt("Do prairie dogs kiss?\nY or N?").toLowerCase();
-console.log("user answer4 is: " + answer4);
-if (answer4 == "y" || answer4 === "yes") {
-  alert("Correct! They speak french too.");
-  correctTimes ++;
-} else if (answer4 === "n" || answer4 === "no") {
-  alert("Wrong. You are just jealous?")
-} else {
-  alert("You didn't type in Y or N.")
-};
-
-// question 5
-var answer5 = prompt("Finally, can cats taste sugar?\nY or N?").toLowerCase();
-console.log("user answer5 is: " + answer5);
-if (answer5 === "n" || answer5 === "no") {
-  alert("Correct! They are flawed, god forbid!");
-  correctTimes ++;
-} else if (answer5 === "yes" || answer5 === "y") {
-  alert("Wrong. They never smile, can't you see.")
-} else {
-  alert("You didn't type in Y or N.")
-};
+yesOrNoQuestions();
 
 //question 6
 var count = 1;
